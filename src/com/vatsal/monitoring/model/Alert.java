7a1393a20be968948c7954ed86a9d150e773e425
Vatsal Rathi <ds099315@gmail.com>
2026-07-06 16:09:24 +0530

    alert model added

--- src/com/vatsal/monitoring/model/Alert.java
+++ src/com/vatsal/monitoring/model/Alert.java
@@ -1,4 +1,66 @@
 package com.vatsal.monitoring.model;
 
+import com.vatsal.monitoring.enums.AlertSeverity;
+
+import java.time.LocalDateTime;
+
 public class Alert {
+
+    private final int alertId;
+    private final int serverId;
+    private final String serverName;
+    private final AlertSeverity severity;
+    private final String message;
+    private final LocalDateTime timestamp;
+
+    public Alert(int alertId,
+                 int serverId,
+                 String serverName,
+                 AlertSeverity severity,
+                 String message,
+                 LocalDateTime timestamp) {
+
+        this.alertId = alertId;
+        this.serverId = serverId;
+        this.serverName = serverName;
+        this.severity = severity;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getAlertId() {
+        return alertId;
+    }
+
+    public int getServerId() {
+        return serverId;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public AlertSeverity getSeverity() {
+        return severity;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" +
+                "alertId=" + alertId +
+                ", serverId=" + serverId +
+                ", serverName='" + serverName + '\'' +
+                ", severity=" + severity +
+                ", message='" + message + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
